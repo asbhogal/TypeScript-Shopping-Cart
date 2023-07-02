31f017384bbdda94efae7846b09a22005f1beadf
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-07-02 23:44:15 +0100

    added header content and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,68 @@
-import { Link } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Icon,
+  Link,
+  List,
+  ListItem,
+  Spacer,
+  UnorderedList,
+} from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
 
 export function Navbar() {
   return (
-    <header className="header">
-      <Link to="/" as={NavLink}>
-        Home
-      </Link>
-      <Link to="/about" as={NavLink}>
-        About
-      </Link>
-      <Link to="/shop" as={NavLink}>
-        Shop
-      </Link>
-    </header>
+    <Box boxShadow="0px 2px 4px rgba(0, 0, 0, 0.2)" p={4}>
+      <header className="header">
+        <Flex>
+          <UnorderedList styleType="none" margin="0">
+            <Flex minWidth="max-content" alignItems="center" gap="2">
+              <ListItem>
+                <Link
+                  to="/"
+                  as={NavLink}
+                  fontSize={24}
+                  _hover={{
+                    textDecoration: "none",
+                    color: "grey",
+                  }}
+                >
+                  Home
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  to="/about"
+                  as={NavLink}
+                  fontSize={24}
+                  _hover={{
+                    textDecoration: "none",
+                    color: "grey",
+                  }}
+                >
+                  About
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  to="/shop"
+                  as={NavLink}
+                  fontSize={24}
+                  _hover={{
+                    textDecoration: "none",
+                    color: "grey",
+                  }}
+                >
+                  Shop
+                </Link>
+              </ListItem>
+            </Flex>
+          </UnorderedList>
+          <Spacer />
+          <Icon as={BsCart3} boxSize={8} />
+        </Flex>
+      </header>
+    </Box>
   );
 }
