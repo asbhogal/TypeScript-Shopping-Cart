16fe6badd594f75e8812321b3617c2788fefbddd
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-07-04 19:11:47 +0100

    added responsive components, rendered JSON data

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,3 +1,14 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import productItems from "../data/products.json";
+
 export function Shop() {
-  return <h1>Shop</h1>;
+  return (
+    <>
+      <SimpleGrid minChildWidth="120px" spacing="20px">
+        {productItems.map((item) => (
+          <Box>{JSON.stringify(item)}</Box>
+        ))}
+      </SimpleGrid>
+    </>
+  );
 }
